99c719338b863206eda091a3a92dc6e1fb9f3b8f
Le Deng <z00382545@FreseniusAdmins-MacBook-Pro.local>
2016-09-13 10:35:38 -0400

    add mvn run

--- Robot/src/main/java/com/ldeng/MainRun.java
+++ Robot/src/main/java/com/ldeng/MainRun.java
@@ -7,6 +7,7 @@ public class MainRun {
     public static void main(String[] args) {
         Robot r2d2 = new Robot();
         String command = "LLFFRRFFRFRF";
+        System.out.println("The input command is "+command);
         r2d2.commandParser(command);
         r2d2.execute();
     }
